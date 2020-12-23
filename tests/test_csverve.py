b08439d7c38585456d98fe56f03dadc1dd0ac49c
eliko1991 <havasove@mskcc.org>
2020-12-23 11:39:13 -0500

    comment out unused tests

--- tests/test_csverve.py
+++ tests/test_csverve.py
@@ -872,8 +872,8 @@ class TestCsverve(unittest.TestCase):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'csverve.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        # assert result.exit_code == 0
+        # assert 'csverve.cli.main' in result.output
+        # help_result = runner.invoke(cli.main, ['--help'])
+        # assert help_result.exit_code == 0
+        # assert '--help  Show this message and exit.' in help_result.output
