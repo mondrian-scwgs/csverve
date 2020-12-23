fcf99a2908fa51fca88ebb28ccfc19e13acf3f24
eliko1991 <havasove@mskcc.org>
2020-12-23 13:33:45 -0500

    fix travis, change version

--- setup.py
+++ setup.py
@@ -27,15 +27,14 @@ test_requirements = [ ]
 
 setup(
     author="Shah Lab",
-    author_email='audreyr@example.com',
-    python_requires='>=3.5',
+    author_email='todo@todo.com',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -57,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mondrian-scwgs/csverve',
-    version='0.1.0',
+    version='0.1.2',
     zip_safe=False,
 )
