1dd3cde7e461d354a7c91539c09d145ec9f65254
Diljot Grewal <diljot.grewal@gmail.com>
2021-12-22 10:57:24 -0500

    version bump

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mondrian-scwgs/csverve',
-    version="v0.2.0",
+    version="v0.2.1",
     zip_safe=False,
 )
