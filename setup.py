1bac57ade9064f09c489128b54353249110a64a4
Diljot <diljot.grewal@gmail.com>
2022-03-18 09:14:45 -0700

    v0.2.5: misspelled cmdline arg (#24)

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mondrian-scwgs/csverve',
-    version="v0.2.4",
+    version="v0.2.5",
     zip_safe=False,
 )
